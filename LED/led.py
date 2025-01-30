1ab2858aff8e557f15faa8939bda0d6d0bea86ce
ThongTruong <126312478+truonghoangthong@users.noreply.github.com>
2025-01-30 23:56:08 +0200

    led and temperature_humidity added

---
+++ LED/led.py
@@ -0,0 +1,44 @@
+import network
+import urequests
+import machine
+import time
+
+LED_PIN = 17 
+led = machine.Pin(LED_PIN, machine.Pin.OUT)
+
+SSID = "connection"
+PASSWORD = "@@@@1234"
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(SSID, PASSWORD)
+
+while not wlan.isconnected():
+    pass  
+
+print("Connected to WiFi")
+
+def get_led_status():
+    url = "http://8.215.20.85/api/v1/get-led-status"
+    
+    try:
+        response = urequests.get(url)
+        data = response.json()
+        response.close()
+        
+        if "status" in data:
+            if data["status"] == "on":
+                led.value(1)  
+                print("LED ON")
+            else:
+                led.value(0)  
+                print("LED OFF")
+        else:
+            print("not found")
+
+    except Exception:
+        print("error")
+
+while True:
+    get_led_status()
+    time.sleep(0.1)  
---
+++ temperature_humidity/temperature_humidity.py
@@ -0,0 +1,70 @@
+import time
+import urequests as requests
+from machine import Pin
+import dht
+import network
+
+# Wi-Fi Details
+SSID = "connection"
+PASSWORD = "@@@@1234"
+
+# REST API Details
+BASE_URL = "http://8.215.20.85/api/v1"
+DATA_ENDPOINT_TEM = "/embed-temperature"
+DATA_ENDPOINT_HUM = "/embed-humidity"
+DEBUG = True
+
+SENSOR = dht.DHT11(Pin(0)) 
+LED = Pin("LED", Pin.OUT)  
+
+def connect_wifi(ssid: str, password: str) -> network.WLAN:
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while not wlan.isconnected():
+        print(".", end="")
+        time.sleep(0.5)
+    return wlan
+
+def send_tem(endpoint: str, data_type: str, value: float) -> None:
+    url = f"{BASE_URL}{endpoint}?{data_type}={value}"
+    try:
+        response = requests.get(url)
+        response.close()
+        print(f"Sent {data_type}: {value}")
+    except Exception :
+        print(f"Error sending data")
+        
+def send_hum(endpoint: str, data_type: str, value: float) -> None:
+    url = f"{BASE_URL}{endpoint}?{data_type}={value}"
+    try:
+        response = requests.get(url)
+        response.close()
+        print(f"Sent {data_type}: {value}")
+    except Exception :
+        print(f"Error sending data")
+
+def main() -> None:
+    print("Program starting.")
+
+    while True:
+        try:
+            SENSOR.measure()
+            temp = SENSOR.temperature()
+            humidity = SENSOR.humidity()
+
+            print(f"Temperature: {temp} °C")
+            print(f"Humidity: {humidity} %")
+
+            LED.on()
+            send_tem(DATA_ENDPOINT_TEM, "temperature", temp)
+            send_hum(DATA_ENDPOINT_HUM, "humidity", humidity)
+            LED.off()
+
+            time.sleep(10)  
+        except Exception :
+            print("Error")
+            time.sleep(5)  
+
+if __name__ == "__main__":
+    main()
