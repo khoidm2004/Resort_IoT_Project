e0d80a10201bf4734ec6e16f485c5d1281a5cf04
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2025-03-27 22:46:57 +0200

    Update slots height

--- src/components/header/loginHeader.jsx
+++ src/components/header/loginHeader.jsx
@@ -1,24 +1,10 @@
-import React, { useEffect } from "react";
 import './header.css';
 
 const LoginHeader = ({ isAdmin }) => { 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <>
-      <div className={`header ${!isAdmin ? 'guest' : ''}`}>
-        <img className="resortlogo" src="/logo.png" alt="logo" />
-      </div>
-    </>
+    <div className={`header ${!isAdmin ? 'guest' : ''}`}>
+      <img className="resortlogo" src="/logo.png" alt="logo" />
+    </div>
   );
 };
 
