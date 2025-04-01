9df077b9b0fec88dc2761f0b4235e4314f013b7a
ThongTruong <126312478+truonghoangthong@users.noreply.github.com>
2025-04-01 13:23:31 +0300

    Update Admin_route.mjs

--- src/server/Admin_route.mjs
+++ src/server/Admin_route.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 const admin_router = express.Router();
 
 admin_router.get('/', (req, res) => {
-    res.send(`Welcome Khoi Do gay`);
+    res.send(`Welcome`);
 });
 
 export default admin_router;
