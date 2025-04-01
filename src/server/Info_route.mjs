96cd019397536031c687c4335357c2436128e1dd
ThongTruong <126312478+truonghoangthong@users.noreply.github.com>
2025-04-01 13:24:02 +0300

    Update Info_route.mjs

--- src/server/Info_route.mjs
+++ src/server/Info_route.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 const info_router  = express.Router();
 
 info_router.get('/', (req, res) => {
-    res.send(`Welcome ,Khoi Do gay`);
+    res.send(`Welcome`);
 });
 
 export default info_router;
