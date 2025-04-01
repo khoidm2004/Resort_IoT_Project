d561a3cbb82ea2c404793172b15e9dce86a721d0
ThongTruong <126312478+truonghoangthong@users.noreply.github.com>
2025-04-01 13:24:17 +0300

    Update Sse_route.mjs

--- src/server/Sse_route.mjs
+++ src/server/Sse_route.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 const sse_router  = express.Router();
 
 sse_router.get('/', (req, res) => {
-    res.send(`Welcome,Khoi Do gay`);
+    res.send(`Welcome`);
 });
 
 export default sse_router;
